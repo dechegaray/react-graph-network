07efc822464089eab80a1e15ec7c1e2ba43de164
Daniel Echegaray <danielechegaray180@gmail.com>
2023-10-18 19:48:52 -0400

    Fix jobs and linters

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
+# network-graph
+
+> Made with create-react-library
+
+[![NPM](https://img.shields.io/npm/v/network-graph.svg)](https://www.npmjs.com/package/network-graph) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install @dechegaray/react-graph-network
+```
+
+## Usage
+
+```tsx
+import React from 'react'
+import { NetworkGraph } from '@dechegaray/react-graph-network'
+
+const viewportHeight = 300
+
+const exampleData = {
+  nodes: [
+    { id: 'node1', label: 'Node 1' },
+    { id: 'node2', label: 'Node 2' },
+    { id: 'node3', label: 'Node 3' },
+  ],
+  links: [
+    { source: 'node1', target: 'node2' },
+    { source: 'node1', target: 'node3' },
+  ],
+}
+
+function App() {
+  return (
+    <div style={{ height: viewportHeight }}>
+      <NetworkGraph id='network-graph' data={exampleData} />
+    </div>
+  )
+}
+```
+
 # Development
 
 For development simply cd into the root folder of this project and run the following:
@@ -13,3 +54,7 @@ After your changes are completed
 - `npm run dev` to start your dev server and see your changes live
 
 Add proper unit/component test as needed
+
+## License
+
+MIT © [](https://github.com/)
--- src/network-graph/types.ts
+++ src/network-graph/types.ts
@@ -1,89 +1,70 @@
-import type {
-  forceSimulation,
-  SimulationLinkDatum,
-  SimulationNodeDatum,
-} from "d3-force";
-import type { ReactElement } from "react";
-import type { BaseType, Selection } from "d3-selection";
+import type { forceSimulation, SimulationLinkDatum, SimulationNodeDatum } from 'd3-force'
+import type { ReactElement } from 'react'
+import type { BaseType, Selection } from 'd3-selection'
 
-export type NodeId = string | number;
+export type NodeId = string | number
 
 export interface BaseNode extends SimulationNodeDatum {
-  id: NodeId;
-  gForce?: number;
+  id: NodeId
+  gForce?: number
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface BaseLink<NodeDatum extends BaseNode>
-  extends SimulationLinkDatum<NodeDatum> {}
+export interface BaseLink<NodeDatum extends BaseNode> extends SimulationLinkDatum<NodeDatum> {}
 
 export interface Data<Node extends BaseNode, Link extends BaseLink<Node>> {
-  nodes: Node[];
-  links: Link[];
+  nodes: Node[]
+  links: Link[]
 }
 
 export interface NodeComponentProps<Node extends BaseNode> {
-  node: Node;
-  isActive: boolean;
-  selectNode(): void;
+  node: Node
+  isActive: boolean
+  selectNode(): void
 }
 
 export interface DetailsComponentProps<Node extends BaseNode> {
-  node: Node;
-  unselectNode(): void;
+  node: Node
+  unselectNode(): void
 }
 
-export interface LinkComponentProps<
-  Node extends BaseNode,
-  Link extends BaseLink<Node>
-> {
-  link: Link;
-  className: string;
+export interface LinkComponentProps<Node extends BaseNode, Link extends BaseLink<Node>> {
+  link: Link
+  className: string
 }
 
-export interface GraphProps<
-  Node extends BaseNode = BaseNode,
-  Link extends BaseLink<Node> = BaseLink<Node>
-> {
-  data: Data<Node, Link>;
-  focusNodeBranch: NodeId;
+export interface GraphProps<Node extends BaseNode = BaseNode, Link extends BaseLink<Node> = BaseLink<Node>> {
+  data: Data<Node, Link>
+  focusNodeBranch: NodeId
 
-  id?: string;
-  pullIn?: boolean;
-  enableDrag?: boolean;
-  enableZoomButtons?: boolean;
-  enableMouseWheelZoom?: boolean;
-  nodeDistance?: number;
-  hoverOpacity?: number;
-  selectNode?(node: Node): void;
-  NodeComponent?(props: NodeComponentProps<Node>): ReactElement;
-  LinkComponent?(props: LinkComponentProps<Node, Link>): ReactElement;
-  DetailsComponent?(props: DetailsComponentProps<Node>): ReactElement;
+  id?: string
+  pullIn?: boolean
+  enableDrag?: boolean
+  enableZoomButtons?: boolean
+  enableMouseWheelZoom?: boolean
+  nodeDistance?: number
+  hoverOpacity?: number
+  selectNode?(node: Node): void
+  NodeComponent?(props: NodeComponentProps<Node>): ReactElement
+  LinkComponent?(props: LinkComponentProps<Node, Link>): ReactElement
+  DetailsComponent?(props: DetailsComponentProps<Node>): ReactElement
 }
 
-export type NetworkGraphProps<
-  Node extends BaseNode = BaseNode,
-  Link extends BaseLink<Node> = BaseLink<Node>
-> = Omit<GraphProps<Node, Link>, "focusNodeBranch" | "selectNode">;
+export type NetworkGraphProps<Node extends BaseNode = BaseNode, Link extends BaseLink<Node> = BaseLink<Node>> = Omit<
+  GraphProps<Node, Link>,
+  'focusNodeBranch' | 'selectNode'
+>
 
-export const isNodeDatum = (
-  subject: BaseLink<BaseNode>["source"]
-): subject is BaseNode => {
-  return (subject as BaseNode).id !== undefined;
-};
+export const isNodeDatum = (subject: BaseLink<BaseNode>['source']): subject is BaseNode => {
+  return (subject as BaseNode).id !== undefined
+}
 
-export type Simulation = ReturnType<typeof forceSimulation>;
+export type Simulation = ReturnType<typeof forceSimulation>
 
-export type SelectedNode<GElement extends BaseType = BaseType> = Selection<
-  GElement,
-  BaseNode,
-  SVGSVGElement,
-  unknown
->;
+export type SelectedNode<GElement extends BaseType = BaseType> = Selection<GElement, BaseNode, SVGSVGElement, unknown>
 
 export type SelectedLink<GElement extends BaseType = BaseType> = Selection<
   GElement,
   BaseLink<BaseNode>,
   SVGSVGElement,
   unknown
->;
+>
