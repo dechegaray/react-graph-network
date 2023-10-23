3ac02c515ffec54b73a6af486f24d047c94310f4
Daniel Echegaray <danielechegaray180@gmail.com>
2023-10-23 13:55:40 -0400

    Migrate to Rollup

--- src/network-graph/index.tsx
+++ src/network-graph/index.tsx
@@ -1,34 +1,20 @@
-// import React from "react";
+import { Zoom } from './zoom'
+import { GraphWithZoom } from './graph-with-zoom'
+import { BaseNode, BaseLink, NetworkGraphProps } from './types'
 
-import { Zoom } from "./zoom";
-import { GraphWithZoom } from "./graph-with-zoom";
-import { BaseNode, BaseLink, NetworkGraphProps } from "./types";
-
-export const NetworkGraph = <
-  Node extends BaseNode = BaseNode,
-  Link extends BaseLink<Node> = BaseLink<Node>
->(
-  props: NetworkGraphProps<Node, Link>
+export const NetworkGraph = <Node extends BaseNode = BaseNode, Link extends BaseLink<Node> = BaseLink<Node>>(
+  props: NetworkGraphProps<Node, Link>,
 ) => {
   return (
-    // <React.StrictMode>
     <Zoom
-      className="network-graph"
-      data-testid="network-graph"
+      className='network-graph'
+      data-testid='network-graph'
       enableZoomButtons={props?.enableZoomButtons || false}
       enableMouseWheelZoom={props?.enableMouseWheelZoom || false}
     >
       {({ zoom, height, width }) => {
-        return (
-          <GraphWithZoom<Node, Link>
-            {...props}
-            zoom={zoom}
-            width={width}
-            height={height}
-          />
-        );
+        return <GraphWithZoom<Node, Link> {...props} zoom={zoom} width={width} height={height} />
       }}
     </Zoom>
-    // </React.StrictMode>
-  );
-};
+  )
+}
